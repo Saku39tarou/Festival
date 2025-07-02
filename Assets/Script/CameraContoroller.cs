fef7210801edd0c9862005fd2ba84624a91470b1
tsuki39-admin <hmj2422020@stu.o-hara.ac.jp>
2025-07-02 11:55:52 +0900

    プレイヤーの当たり判定と操作を確認し実装

---
+++ Assets/Script/CameraContoroller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContoroller : MonoBehaviour
+{
+	public float mouseSensitivity = 250f;//感度
+	private float xRotation = 0f;//x軸回転
+
+	private void Start()
+	{
+		//Cursor.lockState = CursorLockMode.Locked;
+	}
+	private void Update()
+	{
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+		//float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+		//xRotation -= mouseY;
+		xRotation = Mathf.Clamp(xRotation, -90f, 90f);//カメラの回転速度
+													  //xRotation = Mathf.Clamp(xRotation, -90f, 90f);//カメラの回転速度
+
+		transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+		transform.parent.Rotate(Vector3.up * mouseX);
+	}
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	Rigidbody rigid;
+	public float speed = 10.0f;
+	public float jump = 5.5f;
+	private bool isjumpPower = false;//できるできないをfalseとtrueで
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		this.rigid = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//前進
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += speed * transform.forward * Time.deltaTime;
+		}
+		//右
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position -= speed * transform.forward * Time.deltaTime;
+		}
+		//左
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position -= speed * transform.right * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += speed * transform.right * Time.deltaTime;
+		}
+
+		//ジャンプ
+		if (Input.GetKeyDown(KeyCode.Space) && !isjumpPower)
+		{
+			rigid.velocity = Vector3.up * jump;//ジャンプ速度
+			isjumpPower = true;
+		}
+	}
+	}
