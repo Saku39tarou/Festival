dadda2d64ce38fd11ec848bb23fc90b48012417a
tsuki39-admin <hmj2422020@stu.o-hara.ac.jp>
2025-08-24 17:33:05 +0900

    すり抜け箇所編集、扉が開いた後の動作確認

---
+++ Assets/Script/DestroyItem2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyItem2 : MonoBehaviour
+{
+	public GameObject Item;
+	// Update is called once per frame
+	private void OnCollisionEnter(Collision collision)//衝突したら・・・
+	{
+		if (collision.gameObject.CompareTag("Switch2"))
+		{
+
+			Destroy(Item, 3.0f);
+			Debug.Log("扉開いた");
+
+		}
+	}
+}
---
+++ Assets/Script/DoorOpen4.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen4 : MonoBehaviour
+{
+	public GameObject door;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Item2"))
+		{
+			door.GetComponent<DoorSwitch>().enabled = true;
+
+			this.gameObject.SetActive(false);
+		}
+	}
+}
